ff5469788d49ac28c934b8758621e2fa1e3cd5b4
Ardra-worksrepo <ardraasok@yahoo.in>
2020-12-10 11:03:55 +1100

    Added new Js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+<script>
+
+    const taskName = document.querySelector('#newTaskName');
+    const taskDescription = document.querySelector('#newTaskDescription');
+
+    
+
+  
+        
+   
+
+</script>
