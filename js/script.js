75b9ffb3c229469d2e53f2cb0c861fbe983a7984
Ardra-worksrepo <ardraasok@yahoo.in>
2020-12-12 02:47:11 +1100

    final commit

--- js/script.js
+++ js/script.js
@@ -1,94 +1,81 @@
 const newtaskForm = document.querySelector('#formId');
 const msgDisplay = document.querySelector('#alertmessage');
+const msgDisplay1 = document.querySelector('#successmessage');
 msgDisplay.style.display = 'none';
-
-// function for the name validation 
-
-function nameValidation(data) {
-if (data.trim() == "") {
-    msgDisplay.style.display ='block';
-    msgDisplay.innerHTML = 'Task Name Cant Be Empty';
-  } else if (isNaN(data) == false){
-    msgDisplay.style.display = 'block';
-    msgDisplay.innerHTML = 'Task Name Cannot Be Number ONLY!';
-  }else {
-    msgDisplay.style.display ='none';
-  }
-}
-
-// function for the AssignTo validation 
-    function AssigntoValidate(data){
-        if(data == "")
-        {
-            msgDisplay.style.display = 'block';
-            msgDisplay.innerHTML = 'Please Assign a Person for the task';
-        }
-        else {
-                msgDisplay.style.display ='none';
-        }
-
-    }
-
-    // function for the status validation 
-         function statusValidate(data){
-            
-        if(data == "" || data == null){
-            msgDisplay.style.display = 'block';
-            msgDisplay.innerHTML = 'Task status needs to be selected';
-        } 
-            // optional just for checking task for validation
-        else{
-            if(data == "TO DO")
-            {
-                msgDisplay.style.display = 'block';
-                msgDisplay.innerHTML ='Task is initialised';
-            }
-            else if(data == "PROGRESS"){
-                msgDisplay.style.display = 'block';
-                msgDisplay.innerHTML ='We are working on that';
-            }
-            else if(data == "REVIEW"){
-                msgDisplay.style.display = 'block';
-                msgDisplay.innerHTML ='Task is on review';
-           }
-           else if(data == "DONE"){
-            msgDisplay.style.display = 'block';
-            msgDisplay.innerHTML ='Task is Done';
-       }
+msgDisplay1.style.display = 'none';
 
 
-        }
-    } 
-// function for the description validation 
-
-        function descriptionValidate(data){
-        if(data.trim()==""|| data == null)
-        {
-            msgDisplay.style.display = 'block';
-            msgDisplay.innerHTML = 'Task Description cant be empty';
-        }
-        else {
-                msgDisplay.style.display ='none';
-        }
-
-        }   
-    
-    function count_up(obj){
-        document.getElementById('count1').innerHTML= obj.value.length;
-
-    }
-  newtaskForm.addEventListener('submit', (event) => {
+newtaskForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    const taskName = document.querySelector("#newTaskName").value;
-    const taskDescription = document.querySelector("#newTaskDescription").value;
-    const taskAssignedTo = document.querySelector('#assinedTo').value;
-    const taskStatus = document.querySelector('#status').value;
+    const taskName = document.querySelector("#newTaskName");
+    const taskDescription = document.querySelector("#newTaskDescription");
+    const taskAssignedTo = document.querySelector('#assinedTo');
+    const taskStatus = document.querySelector('#status');
+
+       const tName = taskName.value.trim();
+       const tDescription =  taskDescription.value.trim();
+       const tAssignedTo = taskAssignedTo.value;
+       const tStatus = taskStatus.value;
+
+
     // date validation is done in html
     
     
-    
+    if (!validFormFieldInput(tName)){
+        msgDisplay.style.display ='block';
+        msgDisplay.innerHTML = 'Task Name Cant Be Empty';
 
-   
+    }
+    else if(!isNaN(tName)){
+        msgDisplay.style.display ='block';
+        msgDisplay.innerHTML = 'Task Name Cant Be Empty';
+    }
+    else if(tAssignedTo == ""){
+            msgDisplay.style.display = 'block';
+            msgDisplay.innerHTML = 'Please Assign a Person for the task';
+    }
+    else if(tStatus=="")
+    {
+        msgDisplay.style.display = 'block';
+        msgDisplay.innerHTML = 'Task status needs to be selected';
+    }
+    
+    else if (!validFormFieldInput(tDescription))
+    {
+        msgDisplay.style.display = 'block';
+        msgDisplay.innerHTML = 'Task Description cant be empty';
+    }
+    else {
+        msgDisplay.style.display ='none';
+}
+
+            /* Task Status Validation */
+/* if(tStatus == "TO DO")
+    {
+        msgDisplay.style.display = 'block';
+        msgDisplay1.innerHTML ='Task is initialised';
+    }
+     else if(tStatus == "PROGRESS"){
+        msgDisplay.style.display = 'block';
+        msgDisplay1.innerHTML ='We are working on that';
+    }
+    else if(tStatus == "REVIEW"){
+        msgDisplay1.style.display = 'block';
+        msgDisplay1.innerHTML ='Task is on review';
+   }
+    else if(tStatus == "DONE"){
+        msgDisplay1.style.display = 'block';
+        msgDisplay1.innerHTML ='Task is Done';
+    } 
+  */
 });
- 
+
+function validFormFieldInput(data){
+    return data !== null && data !== '';
+}
+
+function count_up(obj){
+    document.getElementById('count1').innerHTML= obj.value.length;
+
+}
---
+++ js/testdemo.js
@@ -0,0 +1,95 @@
+const newtaskForm = document.querySelector('#formId');
+const msgDisplay = document.querySelector('#alertmessage');
+const msgDisplay1 = document.querySelector('#successmessage');
+msgDisplay.style.display = 'none';
+msgDisplay.style.display = 'none';
+// function for the name validation 
+
+function nameValidation(data) {
+if (data.trim() == "") {
+    msgDisplay.style.display ='block';
+    msgDisplay.innerHTML = 'Task Name Cant Be Empty';
+  } else if (isNaN(data) == false){
+    msgDisplay.style.display = 'block';
+    msgDisplay.innerHTML = 'Task Name Cannot Be Number ONLY!';
+  }else {
+    msgDisplay.style.display ='none';
+  }
+}
+
+// function for the AssignTo validation 
+    function AssigntoValidate(data){
+        if(data == "")
+        {
+            msgDisplay.style.display = 'block';
+            msgDisplay.innerHTML = 'Please Assign a Person for the task';
+        }
+        else {
+                msgDisplay.style.display ='none';
+        }
+
+    }
+
+    // function for the status validation 
+         function statusValidate(data){
+            
+        if(data == "" || data == null){
+            msgDisplay.style.display = 'block';
+            msgDisplay.innerHTML = 'Task status needs to be selected';
+        } 
+            // optional just for checking task for validation
+        else{
+            if(data == "TO DO")
+            {
+                msgDisplay.style.display = 'block';
+                msgDisplay.innerHTML ='Task is initialised';
+            }
+            else if(data == "PROGRESS"){
+                msgDisplay.style.display = 'block';
+                msgDisplay.innerHTML ='We are working on that';
+            }
+            else if(data == "REVIEW"){
+                msgDisplay.style.display = 'block';
+                msgDisplay.innerHTML ='Task is on review';
+           }
+           else if(data == "DONE"){
+            msgDisplay.style.display = 'block';
+            msgDisplay.innerHTML ='Task is Done';
+       }
+
+
+        }
+    } 
+// function for the description validation 
+
+        function descriptionValidate(data){
+        if(data.trim()==""|| data == null)
+        {
+            msgDisplay.style.display = 'block';
+            msgDisplay.innerHTML = 'Task Description cant be empty';
+        }
+        else {
+                msgDisplay.style.display ='none';
+        }
+
+        }   
+    
+    function count_up(obj){
+        document.getElementById('count1').innerHTML= obj.value.length;
+
+    }
+  newtaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const taskName = document.querySelector("#newTaskName").value;
+    const taskDescription = document.querySelector("#newTaskDescription").value;
+    const taskAssignedTo = document.querySelector('#assinedTo').value;
+    const taskStatus = document.querySelector('#status').value;
+    // date validation is done in html
+    
+    
+    
+
+   
+});
+ 
