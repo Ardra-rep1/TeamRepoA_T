afe50859d8de0ebe786bbc7f727b8496bb638514
Ardra-worksrepo <ardraasok@yahoo.in>
2020-12-14 14:05:29 +1100

    Implementing with functions

--- js/script.js
+++ js/script.js
@@ -1,81 +1,49 @@
 const newtaskForm = document.querySelector('#formId');
 const msgDisplay = document.querySelector('#alertmessage');
-const msgDisplay1 = document.querySelector('#successmessage');
 msgDisplay.style.display = 'none';
-msgDisplay1.style.display = 'none';
 
+      
+// function for the description validation 
+    
+    function count_up(obj){
+        document.getElementById('count1').innerHTML= obj.value.length;
 
-newtaskForm.addEventListener('submit', (event) => {
+    }
+  newtaskForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const taskName = document.querySelector("#newTaskName");
     const taskDescription = document.querySelector("#newTaskDescription");
     const taskAssignedTo = document.querySelector('#assinedTo');
     const taskStatus = document.querySelector('#status');
-
-       const tName = taskName.value.trim();
-       const tDescription =  taskDescription.value.trim();
-       const tAssignedTo = taskAssignedTo.value;
-       const tStatus = taskStatus.value;
-
-
-    // date validation is done in html
+    const taskDueDate = document.querySelector('#taskDuedate');
+   
     
+    validateInputs(taskName);
+    validateInputs(taskDueDate);
+    validateInputs(taskAssignedTo);
+    validateInputs(taskStatus);
+    validateInputs(taskDescription);
     
-    if (!validFormFieldInput(tName)){
-        msgDisplay.style.display ='block';
-        msgDisplay.innerHTML = 'Task Name Cant Be Empty';
 
-    }
-    else if(!isNaN(tName)){
-        msgDisplay.style.display ='block';
-        msgDisplay.innerHTML = 'Task Name Cant Be Empty';
-    }
-    else if(tAssignedTo == ""){
-            msgDisplay.style.display = 'block';
-            msgDisplay.innerHTML = 'Please Assign a Person for the task';
-    }
-    else if(tStatus=="")
-    {
-        msgDisplay.style.display = 'block';
-        msgDisplay.innerHTML = 'Task status needs to be selected';
-    }
-    
-    else if (!validFormFieldInput(tDescription))
-    {
-        msgDisplay.style.display = 'block';
-        msgDisplay.innerHTML = 'Task Description cant be empty';
-    }
-    else {
-        msgDisplay.style.display ='none';
-}
-
-            /* Task Status Validation */
-/* if(tStatus == "TO DO")
-    {
-        msgDisplay.style.display = 'block';
-        msgDisplay1.innerHTML ='Task is initialised';
-    }
-     else if(tStatus == "PROGRESS"){
-        msgDisplay.style.display = 'block';
-        msgDisplay1.innerHTML ='We are working on that';
-    }
-    else if(tStatus == "REVIEW"){
-        msgDisplay1.style.display = 'block';
-        msgDisplay1.innerHTML ='Task is on review';
-   }
-    else if(tStatus == "DONE"){
-        msgDisplay1.style.display = 'block';
-        msgDisplay1.innerHTML ='Task is Done';
-    } 
-  */
+   
 });
 
-function validFormFieldInput(data){
-    return data !== null && data !== '';
+
+function validateInputs(data){
+    let dataValue = data.value;
+    let errorMsg;
+if(dataValue.trim()== ""|| dataValue == null)
+{
+    msgDisplay.style.display = 'block';
+    errorMsg = document.createElement('div');
+
+    errorMsg.innerHTML= `${data.name.toUpperCase()} cant be empty`;
+    msgDisplay.appendChild(errorMsg);
+    
+
 }
 
-function count_up(obj){
-    document.getElementById('count1').innerHTML= obj.value.length;
+}   
 
-}
+ 
