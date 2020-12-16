76832a8dd296124d6989d8eba2c1eba1aa7688e6
Ardra <piyaasok@gmail.com>
2020-12-16 13:48:14 +1100

    fixed taskManager.js

---
+++ js/taskmanager.js
@@ -0,0 +1,23 @@
+class TaskManager {
+
+    // the tasks and currentId property in the constructor
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+    // the addTask method
+    addTask(tName, tAssignedTo, tDescription, tDueDate) {
+        const task = {
+            // the currentId property
+            id: this.currentId++,
+            name: tName,
+            dueDate: tDueDate,
+            assignedTo: tAssignedTo,
+            description: tDescription,
+            status: 'TO DO'
+
+        };
+        // Push the task to the tasks array
+        this.tasks.push(task);
+    }
+}
