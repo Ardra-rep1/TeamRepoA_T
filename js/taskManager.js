84f0af90a4b3449297311916d8963aaf27fc4dc7
Ardra <piyaasok@gmail.com>
2020-12-18 11:51:44 +1100

    Add the Done

--- js/taskManager.js
+++ js/taskManager.js
@@ -11,7 +11,7 @@ class TaskManager {
     addTask(tName, tAssignedTo, tDescription, tDueDate) {
         const task = {
             // the currentId property
-
+            id: this.currentId++,
             name: tName,
             dueDate: tDueDate,
             assignedTo: tAssignedTo,
@@ -24,6 +24,24 @@ class TaskManager {
 
 
     }
+    getTaskId(taskId) {
+
+        let foundTaskId;
+        for (let i = 0; i < this.tasks.length; i++) {
+
+            const task = this.tasks[i];
+
+            if (task.id == taskId) {
+
+                foundTaskId = task;
+            }
+        }
+
+        return foundTaskId;
+    }
+
+
+
 
 
     //function to render the tasks array on the screen 
@@ -33,28 +51,39 @@ class TaskManager {
         const newCardPlace = document.querySelector('#taskDisplayList');
         const cardCopy = document.querySelector('#newtaskCard');
         newCardPlace.innerHTML = "";
-
+     
         this.tasks.forEach(task => {
-
+            
             const cardCopyClone = cardCopy.cloneNode(true);
+
+
 
             cardCopyClone.children[0].innerText = `Assignee:  ${task.assignedTo} `;
 
 
             cardCopyClone.children[1].firstElementChild.innerText = `Task Name:  ${task.name}`;
             cardCopyClone.children[1].children[1].innerText = `Task Discription:  ${task.description}`;
-
+            cardCopyClone.children[1].children[2].innerText = `Task Status:  ${task.status}`;
+            cardCopyClone.children[1].children[3].id = `${task.id}`;
             cardCopyClone.children[2].innerText = `Due Date: ${task.dueDate}`;
+            
             let newLi = document.createElement('li');
+           
+            
             newLi.appendChild(cardCopyClone);
             newLi.className = 'list-inline-item col-9';
-
             newCardPlace.appendChild(newLi);
+           
+            
+
         }
+        
 
         )
 
     }
-
 }
+
+
+
 
