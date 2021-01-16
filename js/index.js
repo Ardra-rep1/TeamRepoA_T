2b144bebf41c2eab4beb408e861abca70ad7e9ab
Tek <edx.tek@gmail.com>
2021-01-16 20:03:54 +1100

    Delete button work

--- js/index.js
+++ js/index.js
@@ -1,125 +1,108 @@
-
 // Initializing a new TaskManager with currentId set to 0
 const taskManager = new TaskManager(0);
 
-const newtaskForm = document.querySelector('#formId');
-const msgDisplay = document.querySelector('#alertmessage');
-msgDisplay.style.display = 'none';
+const newtaskForm = document.querySelector("#formId");
+const msgDisplay = document.querySelector("#alertmessage");
+msgDisplay.style.display = "none";
 
 const taskName = document.querySelector("#newTaskName");
 const taskDescription = document.querySelector("#newTaskDescription");
-const taskAssignedTo = document.querySelector('#assignedTo');
-const taskStatus = document.querySelector('#status');
-const taskDueDate = document.querySelector('#taskDueDate');
-
+const taskAssignedTo = document.querySelector("#assignedTo");
+const taskStatus = document.querySelector("#status");
+const taskDueDate = document.querySelector("#taskDueDate");
 
 function count_up(obj) {
-    document.getElementById('count1').innerHTML = obj.value.length;
-
+  document.getElementById("count1").innerHTML = obj.value.length;
 }
 
-// Event handler to listen the submit event from the newwtask html page 
-newtaskForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+// Event handler to listen the submit event from the newwtask html page
+newtaskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    msgDisplay.innerHTML = '';
+  msgDisplay.innerHTML = "";
 
+  let vnc = validateInputs(taskName);
+  let vddc = validateInputs(taskDueDate);
+  let vac = validateInputs(taskAssignedTo);
+  validateInputs(taskStatus);
+  let vdc = validateInputs(taskDescription);
 
-    let vnc = validateInputs(taskName);
-    let vddc = validateInputs(taskDueDate);
-    let vac = validateInputs(taskAssignedTo);
-    validateInputs(taskStatus);
-    let vdc = validateInputs(taskDescription);
+  let taskFilterResult = taskFilterPush(vnc, vddc, vac, vdc);
 
-    let taskFilterResult = taskFilterPush(vnc, vddc, vac, vdc);
-
-    if (!(taskFilterResult == false)) {
-
-        msgDisplay.style.display = 'none';
-        taskInputRefresh(taskName, taskDescription, taskAssignedTo, taskDueDate)
-        taskManager.render();
-
-
-    }
+  if (!(taskFilterResult == false)) {
+    msgDisplay.style.display = "none";
+    taskInputRefresh(taskName, taskDescription, taskAssignedTo, taskDueDate);
+    taskManager.render();
+  }
 });
 
-
-// function to check the data for empty string and null value and return false if it is 
+// function to check the data for empty string and null value and return false if it is
 
 function validateInputs(data) {
-    let dataValue = data.value;
-    let errorMsg;
+  let dataValue = data.value;
+  let errorMsg;
 
-    if (dataValue.trim() == "" || dataValue == null) {
-        msgDisplay.style.display = 'block';
-        errorMsg = document.createElement('div');
-        errorMsg.innerHTML = `${data.name.toUpperCase()} CANNOT BE EMPTY`;
-        msgDisplay.appendChild(errorMsg);
-        return false;
-    }
+  if (dataValue.trim() == "" || dataValue == null) {
+    msgDisplay.style.display = "block";
+    errorMsg = document.createElement("div");
+    errorMsg.innerHTML = `${data.name.toUpperCase()} CANNOT BE EMPTY`;
+    msgDisplay.appendChild(errorMsg);
+    return false;
+  }
 }
 
 // Function to add the task to the task array ( in taskManager js )
 function taskFilterPush(vnc, vddc, vac, vdc) {
-
-    if (!(vnc == false) && !(vddc == false) && !(vac == false) && !(vdc == false)) {
-        taskManager.addTask(taskName.value, taskAssignedTo.value, taskDescription.value, taskDueDate.value);
-    } else {
-        return false;
-    }
-
+  if (
+    !(vnc == false) &&
+    !(vddc == false) &&
+    !(vac == false) &&
+    !(vdc == false)
+  ) {
+    taskManager.addTask(
+      taskName.value,
+      taskAssignedTo.value,
+      taskDescription.value,
+      taskDueDate.value
+    );
+  } else {
+    return false;
+  }
 }
 
+// Function to clear the input field after the submit button is pressed
 
-
-// Function to clear the input field after the submit button is pressed 
-
-
-function taskInputRefresh(taskName, taskDescription, taskAssignedTo, taskDueDate) {
-    taskName.value = '';
-    taskDescription.value = '';
-    taskAssignedTo.value = '';
-    taskDueDate.value = '';
+function taskInputRefresh(
+  taskName,
+  taskDescription,
+  taskAssignedTo,
+  taskDueDate
+) {
+  taskName.value = "";
+  taskDescription.value = "";
+  taskAssignedTo.value = "";
+  taskDueDate.value = "";
 }
 
-const tasksList = document.querySelector('#taskDisplayList');
-tasksList.addEventListener('click', (ev) => {
-    ev.preventDefault();
+// Function declaration to event handle the  delete  button click  for removing the object from the array and re-render the array list
 
-    if (ev.target.classList.contains('done-button')) {
+const clickHandler = (e) => {
+  if (e.target.matches(".delete-button")) {
+    taskManager.getTaskId(e.target.id);
+    taskManager.render();
+  }
 
-        const parentTask = ev.target.parentElement;
+  if(e.target.matches(".done-button")){
+      
+  }
+};
 
-
-        // console.log(parentTask);
-
-
-        const taskId = Number(parentTask.id);
-        //   console.log(taskId); correct up to this point
-
-
-        const task = taskManager.getTaskId(taskId);
-
-        /// console.log(task);
-
-        //console.log(task);
-        task.status = 'DONE';
-
-
-        taskManager.render();
-    }
-});
+//Function declaration to event handle the Mark as done click to change the status 
 
 
 
-//priority & git 
-// Past dates fix the past dat
-//showing only remaining days.
-// we will focus on the styling later.
+// Selecting the parent element of the list
+const deleteItem = document.querySelector("#taskDisplayList");
 
-// notification/ popup  for the urgent task 
-// improve the validation
-
-
-
-
+// Adding event listener to the parent element which grab any event on the children by event delegation
+deleteItem.addEventListener("click", clickHandler);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,100 +1,96 @@
-
 class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
-    }
-    // the addTask method
-
-
-    addTask(tName, tAssignedTo, tDescription, tDueDate) {
-        const task = {
-            // the currentId property
-
-            id: this.currentId++,
-            name: tName,
-            dueDate: tDueDate,
-            assignedTo: tAssignedTo,
-            description: tDescription,
-            status: 'TO DO'
-        };
-    this.tasks.push(task);
-
-   
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
   }
-    getTaskId(taskId) {
+  // the addTask method
 
-        let foundTaskId;
-        for (let i = 0; i < this.tasks.length; i++) {
+  addTask(tName, tAssignedTo, tDescription, tDueDate) {
+    const task = {
+      // the currentId property
 
-            const task = this.tasks[i];
+      id: this.currentId++,
+      name: tName,
+      dueDate: tDueDate,
+      assignedTo: tAssignedTo,
+      description: tDescription,
+      status: "TO DO",
+    };
+    this.tasks.push(task);
+  }
 
-            if (task.id == taskId) {
-
-                foundTaskId = task;
-            }
-        }
-
-        return foundTaskId;
+  // Function to passing the index number of the objet to the getDelete method
+  getTaskId(taskId) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+      if (task.id == taskId) {
+        console.log(i);
+        this.getDelete(i);
+      }
     }
+  }
 
+  // Function to  remove the object from the tasks array
 
+  getDelete(itemIndex) {
+    this.tasks.splice(itemIndex, 1);
+    if (this.tasks.length < 1) {
+      document.location.reload(true);
+    }
+  }
 
+  // Function to formate the date
 
-
-  
-  // Function to formate the date 
-
-
-  dueDateFormate(dueDate){
-     const taskdueDate = new Date(dueDate);
+  dueDateFormate(dueDate) {
+    const taskdueDate = new Date(dueDate);
     // Format date to be dd/mm/yyyy
-     const formattedDate = taskdueDate.getDate() + '/' + (taskdueDate.getMonth() + 1) + '/' + taskdueDate.getFullYear();
+    const formattedDate =
+      taskdueDate.getDate() +
+      "/" +
+      (taskdueDate.getMonth() + 1) +
+      "/" +
+      taskdueDate.getFullYear();
 
-   return formattedDate;
-}
+    return formattedDate;
+  }
 
-    
+  // function to return the remaining days
 
-// function to return the remaining days 
+  remainingDays(dueDate) {
+    let currentDate = new Date();
+    const currentDateDay = currentDate.getDate();
+    const currentDateMonth = currentDate.getMonth() + 1;
+    const currentDateYear = currentDate.getFullYear();
+    // }
 
-remainingDays(dueDate){
+    // due Date data
 
+    const taskdueDate = new Date(dueDate);
 
-  
-  let currentDate = new Date();
-  const currentDateDay = currentDate.getDate();
-  const currentDateMonth = currentDate.getMonth() + 1;
-  const currentDateYear = currentDate.getFullYear();
-       // }
-        
-  // due Date data 
+    const dueDateDay = taskdueDate.getDate();
+    const dueDateMonth = taskdueDate.getMonth() + 1;
+    const dueDateYear = taskdueDate.getFullYear();
 
-  const taskdueDate = new Date(dueDate);
-  
-  const dueDateDay = taskdueDate.getDate();
-  const dueDateMonth = taskdueDate.getMonth() + 1;
-  const dueDateYear  =  taskdueDate.getFullYear();
-    
+    return `${dueDateDay - currentDateDay} Days ${
+      dueDateMonth - currentDateMonth
+    } Months ${dueDateYear - currentDateYear} Years Remaining`;
+  }
 
-  return `${dueDateDay - currentDateDay} Days ${dueDateMonth - currentDateMonth} Months ${dueDateYear - currentDateYear } Years Remaining`;
-
-
-};
-  
   //function to render the tasks array on the screen
 
   render() {
     // query selecting the task display ul
+
+    
     const newCardPlace = document.querySelector("#taskDisplayList");
     const cardCopy = document.querySelector("#newtaskCard");
+    newCardPlace.classList.remove("hidden-list");
     newCardPlace.innerHTML = "";
 
     this.tasks.forEach((task) => {
-
       const dueDate = task.dueDate;
       const formattedDate = this.dueDateFormate(dueDate);
-      const remainingDays = this.remainingDays(dueDate)
+      const remainingDays = this.remainingDays(dueDate);
       const cardCopyClone = cardCopy.cloneNode(true);
 
       cardCopyClone.children[0].innerText = `Assignee:  ${task.assignedTo} `;
@@ -102,18 +98,17 @@ remainingDays(dueDate){
       cardCopyClone.children[1].firstElementChild.innerText = `${task.name}`;
       cardCopyClone.children[1].children[1].innerText = `${task.description}`;
       cardCopyClone.children[1].children[2].innerText = `Status: ${task.status}`;
-      cardCopyClone.children[1].children[3].id = `${task.id}`;
+      cardCopyClone.children[1].children[4].children[0].id = task.id;
+
       cardCopyClone.children[2].children[0].innerText = `Due Date: ${formattedDate} `;
       cardCopyClone.children[2].children[1].innerText = `Remaining Days: ${remainingDays}`;
-      
 
       let newLi = document.createElement("li");
       newLi.appendChild(cardCopyClone);
-      newLi.className = "list-inline-item col-9";
+
+      newLi.className = "list-inline-item col-9 ";
 
       newCardPlace.appendChild(newLi);
     });
   }
-
 }
-
